aef728bde55a8369068fac348d614338a430025a
walnut <walnut_lht@sjtu.edu.cn>
2024-12-23 18:59:14 +0800

    简单提交，包含新增和删除文件

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,8 @@ void REPL()
         }
         catch (const RuntimeError &RE)
         {
-            std :: cout << RE.message();
-            // std :: cout << "RuntimeError";
+            // std :: cout << RE.message();
+            std :: cout << "RuntimeError";
         }
         puts("");
     }
